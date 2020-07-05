5bef493bcbae26247a28df47ce16f96492fc1637
outgoingbot <outgoingbot@gmail.com>
2020-07-04 20:16:34 -0600

    Inital Commit

--- Release/FastLED/platforms.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Tower\AppData\Local\Temp\VMBuilds\ESP8266_Twitch_Badge_02\esp8266_nodemcuv2\Release\FastLED\platforms.cpp.o: \
- C:\Users\Tower\Documents\Arduino\libraries\FastLED\platforms.cpp
